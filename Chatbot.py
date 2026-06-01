6b41f1b7ab7dff2919dad4b9f52778f4eabefb10
SniperMask <118263662+rex922@users.noreply.github.com>
2026-06-01 23:45:50 +0300

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -10,7 +10,7 @@ with st.sidebar:
 st.title("💬 Chatbot")
 st.caption("🚀 A Streamlit chatbot powered by OpenAI")
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+    st.session_state["messages"] = [{"role": "assistant", "content": "Merhaba, Nasıl Yardımcı Olabilirim?"}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
